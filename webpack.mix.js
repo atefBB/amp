6700719677e886b02285863499164b656233dde9
Atef Ben Ali <atef.bettaib@gmail.com>
2018-10-04 08:58:57 +0100

    add new js files to `webpack.mix.js` file

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,14 @@
 const mix = require('laravel-mix');
 
 mix.scripts([
+    'jquery-1.11.0.min.js',
+    'jquery-latest-1.11.1.min.js',
+    'jquery-chirurgictour.min.js',
+    'ga.min.js',
+    'bootstrap.min.js',
+    'jquery.stellar.min.js',
+    'jquery-ui-1.10.3.custom.js',
+    'html5lightbox.js',
+    'slick.min.js',
     'custom.min.js',
-    'ga.min.js'
 ], 'app.js');
